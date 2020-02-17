8c98a8b7a175d1b4dcb29057791ba5791b6d1c7d
nabab <thomas.nabet@gmail.com>
2020-02-17 08:13:26 +0100

    Added get_user_id, get_admin_group and get_dev_group to user\manager

--- src/mvc/model/_definitions.php
+++ src/mvc/model/_definitions.php
@@ -1,12 +1,14 @@
 <?php
+/** @var array The result */
 $res = [
   'success' => false
 ];
-if ( $model->db->check() ){
+if ($model->db->check()) {
+  $mgr = $model->inc->user->get_manager();
   $res['data'] = [
-    'external_user_id' => $model->db->select_one('bbn_users', 'id', ['email' => BBN_EXTERNAL_USER_EMAIL]),
-    'admin_group' => $model->db->select_one('bbn_users_groups', 'id', ['nom' => 'Administrateurs']),
-    'dev_group' => $model->db->select_one('bbn_users_groups', 'id', ['nom' => 'Développeurs']),
+    'external_user_id' => $mgr->get_user_id(BBN_EXTERNAL_USER_EMAIL),
+    'admin_group' => $mgr->get_admin_group(),
+    'dev_group' => $mgr->get_dev_group()
   ];
   $res['success'] = true;
 }
