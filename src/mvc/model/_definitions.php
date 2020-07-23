7598f31c5484d57731135b4a5c01ab9bfff3598e
Mirko Argentino <mirko@bbn.solutions>
2020-07-23 16:33:12 +0200

    _definitions.php
    Added verification of  the BBN_EXTERNAL_USER_EMAIL constant

--- src/mvc/model/_definitions.php
+++ src/mvc/model/_definitions.php
@@ -6,10 +6,12 @@ $res = [
 if ($model->db->check()) {
   $mgr = $model->inc->user->get_manager();
   $res['data'] = [
-    'external_user_id' => $mgr->get_user_id(BBN_EXTERNAL_USER_EMAIL),
     'admin_group' => $mgr->get_admin_group(),
     'dev_group' => $mgr->get_dev_group()
   ];
+  if ( defined('BBN_EXTERNAL_USER_EMAIL') ){
+    $res['external_user_id'] = $mgr->get_user_id(BBN_EXTERNAL_USER_EMAIL);
+  }
   $res['success'] = true;
 }
 return $res;
