7ce559bd9c77f2b1672c36d2680f4264e0bbf657
Thomas Nabet <thomas.nabet@gmail.com>
2021-02-15 01:31:48 +0100

    Translations, options, fixes

--- src/mvc/model/update/updater.php
+++ src/mvc/model/update/updater.php
@@ -5,15 +5,40 @@
  **/
 
 use bbn\X;
-
 /** @var $model \bbn\mvc\model */
+$fs = new \bbn\File\System();
+$file = $model->libPath().'bbn/bbn/options.json';
+$file2 = $model->libPath().'bbn/bbn/permissions.json';
+$file3 = $model->libPath().'bbn/bbn/plugins.json';
+$appui = X::toArray($fs->decodeContents($file));
+$permissions = X::toArray($fs->decodeContents($file2));
+$plugins = X::toArray($fs->decodeContents($file3));
+array_unshift($appui['items'], $permissions);
+$permissions['id_alias'] = ['permissions', 'appui'];
+$permissions['items'][0]['id_alias'] = ['access', 'permissions', 'appui'];
+$permissions['items'][1]['id_alias'] = ['plugins', 'permissions', 'appui'];
+$permissions['items'][2]['id_alias'] = ['options', 'permissions', 'appui'];
+foreach ($appui['items'] as $i => &$it) {
+  if ($i) {
+    if (!isset($it['items'])) {
+      $it['items'] = [];
+    }
+    $it['items'][] = $plugins;
+    $it['items'][] = $permissions;
+  }
+}
+unset($it);
+
+
 $res = [];
-$o = $model->inc->options;
-$data = $model->getCachedModel('./default_options', 3601);
-$opt = $data['options'];
+$o =& $model->inc->options;
 $id_appui = $o->fromCode('appui');
 $path = [$id_appui];
-$compare = function ($o1, $o2, $is_option) use (&$compare) {
+$operations = [
+  'insert' => [],
+  'update' => []
+];
+$is_same = function ($o1, $o2, $is_option) use (&$compare, &$operations) {
   $diff = [];
   foreach ($o1 as $key => $val) {
     if ($is_option && ($key === 'items')) {
@@ -49,44 +74,112 @@ $compare = function ($o1, $o2, $is_option) use (&$compare) {
 
   return $diff;
 };
-$items = X::map(
-  function ($a) use (&$o, &$path, &$compare) {
-    $tmp = [
-      'text' => $a['code'] ?: '???',
-      'data' => $a
-    ];
-    if (isset($a['code'])) {
-      if (!empty($a['items'])) {
-        $tmp['items'] = $a['items'];
-        $tmp['alt'] = $a['items'];
-        unset($tmp['data']['items']);
-      }
-      $len = count($path);
-      array_unshift($path, $a['code']);
-      if ($tmp['id'] = $o->fromCode(...$path)) {
-        $obj = $o->option($tmp['id']);
-        if ($cfg = $o->getCfg($tmp['id'])) {
-          $obj['cfg'] = $cfg;
-        }
+// $o['id_alias']
+// $o['cfg']['default']
+// $o['cfg']['id_root_alias']
 
-        if ($items = $compare($a, $obj, true)) {
-          $tmp['alt'] = $items;
+$mapper = function($arr, $path = [], $second_go = false) use (&$res, &$o, &$mapper) {
+  $r = [];
+  foreach ($arr as $a) {
+    $cur_path = $path;
+    array_unshift($cur_path, $a['code']);
+    $x = json_encode($cur_path);
+    if ($second_go) {
+      if (!empty($a['id_alias'])) {
+        if (is_array($a['id_alias'])) {
+          if ($a['id_alias'] = $o->fromCode(...$a['id_alias'])) {
+            $x .= ' ALIAS FOUND - ';
+          }
+          else {
+	          $x .= ' ALIAS NOT FOUND !!! - ';
+            unset($a['id_alias']);
+          }
+        }
+        else {
+          $x .= ' ALIAS IS NOT ARRAY !!! - ';
+          unset($a['id_alias']);
+        }
+      }
+      if (!empty($a['cfg']) && !empty($a['cfg']['default'])) {
+        if (is_array($a['cfg']['default'])) {
+          if ($a['cfg']['default'] = $o->fromCode(...$a['cfg']['default'])) {
+            $x .= ' DEFAULT FOUND - ';
+          }
+          else {
+	          $x .= ' DEFAULT NOT FOUND !!! - ';
+            unset($a['cfg']['default']);
+          }
+        }
+        else {
+          unset($a['cfg']['default']);
+        }
+      }
+      if (!empty($a['cfg']) && !empty($a['cfg']['id_root_alias'])) {
+        if (is_array($a['cfg']['id_root_alias'])) {
+          if ($a['cfg']['id_root_alias'] = $o->fromCode(...$a['cfg']['id_root_alias'])) {
+            $x .= ' ROOT ALIAS FOUND - ';
+          }
+          else {
+	          $x .= ' ROOT ALIAS NOT FOUND !!! - ';
+            unset($a['cfg']['id_root_alias']);
+          }
+        }
+        else {
+          unset($a['cfg']['id_root_alias']);
+        }
+      }
+    }
+    else {
+      if (isset($a['id_alias'])) {
+        unset($a['id_alias']);
+      }
+      if (isset($a['cfg']['default'])) {
+        unset($a['cfg']['default']);
+      }
+      if (isset($a['cfg']['id_root_alias'])) {
+        unset($a['cfg']['id_root_alias']);
+      }
+    }
+
+    if ($id = $o->fromCode(...$cur_path)) {
+      $x .= 'OPTION  FOUND';
+      if ($second_go) {
+        if (!empty($a['cfg'])) {
+          $o->setCfg($id, $a['cfg']);
+          unset($a['cfg']);
+        }
+        if (!empty($a['id_alias'])) {
+          $o->set($id, $a);
         }
       }
       else {
-        $tmp['error'] = "no_option";
+        if (isset($a['id_alias'])) {
+          unset($a['id_alias']);
+        }
+        if (isset($a['cfg'])) {
+          unset($a['cfg']);
+        }
+        $o->set($id, $a);
       }
-      array_shift($path);
+    }
+    elseif ($a['id_parent'] = $o->fromCode(array_slice($cur_path, 1))) {
+      $o->add($a);
+      $x .= ' ID_PARENT FOUND ';
     }
     else {
-      $tmp['error'] = 'no_code';
+      $x .= ' OPTION NOT FOUND!!!!!!!!!!!!!!!!!!!!!!!!!';
     }
+    if (!empty($a['items'])) {
+      $mapper($a['items'], $cur_path, $second_go);
+    }
+    $res[] = $x;
+  }
+};
+$mapper([$appui]);
+$res[] = '---------------------------------------------------------------';
+$res[] = '---------------------------------------------------------------';
+$res[] = '---------------------------------------------------------------';
+$mapper([$appui], [], true);
 
-    //$a['result'] = $tmp;
-    return $tmp;
-  },
-  $opt['items'],
-  'items'
-);
-
-return ['options' => $items];
+die(X::dump($res));
+return ['options' => $res];
