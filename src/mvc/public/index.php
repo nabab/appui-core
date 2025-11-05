feb47b30ef59853feb30f7362d85347f27e9db68
nabab <thomas.nabet@gmail.com>
2025-11-05 12:28:17 +0100

    Made index data go to user cache

--- src/mvc/public/index.php
+++ src/mvc/public/index.php
@@ -9,59 +9,70 @@
 use bbn\X;
 
 /** @var bbn\Mvc\Controller $ctrl */
-$routes = $ctrl->getRoutes();
-$plugins = [];
-$slots = [
-  'before' => [],
-  'headleft' => [],
-  'head' => [],
-  'headright' => [],
-  'central' => [],
-  'status' => [],
-  'after' => []
-];
 
-foreach ($routes as $r) {
-  $plugins[$r['name']] = $r['url'];
 
-  if ($appuiElements = $ctrl->getSubpluginModelGroup('app-ui', $r['name'], 'appui-core')) {
-    foreach ($appuiElements as $obj) {
-      foreach ($obj as $slot => $data) {
-        if (isset($slots[$slot])) {
-          array_push($slots[$slot], ...(X::isAssoc($data) ? [$data] : $data));
+if ($ctrl->inc->user->check()) {
+  $cacheName = 'appui-core-index';
+  if ($ctrl->inc->user->hasCache($cacheName)) {
+    $data = $ctrl->inc->user->getCache($cacheName);
+  }
+  else {
+    $routes = $ctrl->getRoutes();
+    $plugins = [];
+    $slots = [
+      'before' => [],
+      'headleft' => [],
+      'head' => [],
+      'headright' => [],
+      'central' => [],
+      'status' => [],
+      'after' => []
+    ];
+
+    foreach ($routes as $r) {
+      $plugins[$r['name']] = $r['url'];
+
+      if ($appuiElements = $ctrl->getSubpluginModelGroup('app-ui', $r['name'], 'appui-core')) {
+        foreach ($appuiElements as $obj) {
+          foreach ($obj as $slot => $data) {
+            if (isset($slots[$slot])) {
+              array_push($slots[$slot], ...(X::isAssoc($data) ? [$data] : $data));
+            }
+          }
         }
+        //X::ddump("YYY", $slots);
       }
     }
-    //X::ddump("YYY", $slots);
-  }
-}
 
-foreach ($slots as &$s) {
-  foreach ($s as &$m) {
-    if (!isset($m['priority'])) {
-      $m['priority'] = 5;
+    foreach ($slots as &$s) {
+      foreach ($s as &$m) {
+        if (!isset($m['priority'])) {
+          $m['priority'] = 5;
+        }
+      }
+
+      unset($m);
+      X::sortBy($s, 'priority');
     }
+    unset($s);
+
+    $data = $ctrl->getModel($ctrl->pluginUrl('appui-core').'/_index');
+    $data['plugins'] = $plugins;
+    $data['slots'] = $slots;
+    $ctrl->inc->user->setCache($cacheName, $data, 86400);
   }
 
-  unset($m);
-  X::sortBy($s, 'priority');
-}
-unset($s);
-
-$ctrl->data = $ctrl->getModel($ctrl->pluginUrl('appui-core').'/_index');
-$ctrl->addData([
-  'plugins' => $plugins,
-  'slots' => $slots,
-]);
-// The whole DOM
-if (empty($ctrl->post)) {
-  $ctrl->data['custom_css'] = $ctrl->customPluginView('index', 'css', [], 'appui-core') ?: $ctrl->getLess();
-  $ctrl->data['token'] = $ctrl->inc->user->addToken();
-  $ctrl->combo($ctrl->data['site_title'], true);
-}
-// Only the data
-else {
-  $ctrl->addJs();
-  $ctrl->data['js_data'] = $ctrl->customPluginView('index', 'js', $ctrl->data, 'appui-core');
-  $ctrl->obj->data = $ctrl->data;
+  $ctrl->addData($data);
+  // The whole DOM
+  if (empty($ctrl->post)) {
+    $ctrl->data['custom_css'] = $ctrl->customPluginView('index', 'css', [], 'appui-core') ?: $ctrl->getLess();
+    $ctrl->data['token'] = $ctrl->inc->user->addToken();
+    $ctrl->combo($ctrl->data['site_title'], true);
+  }
+  // Only the data
+  else {
+    $ctrl->addJs();
+    $ctrl->data['js_data'] = $ctrl->customPluginView('index', 'js', $ctrl->data, 'appui-core');
+    $ctrl->obj->data = $ctrl->data;
+  }
 }
